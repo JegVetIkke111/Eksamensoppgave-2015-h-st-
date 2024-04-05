b54e92e4564700098dc61a599dd6efcd35915602
Boink <98953747+JegVetIkke111@users.noreply.github.com>
2024-04-05 17:49:15 +0200

    save

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let overskrift = document.CreateElement("h1");
 let overskriftInnhold = document.createTextNode("Regn ut om du har nok snitt for å komme inn på ditt studiepoeng")
 
-
 //Lager tre brukerinputt
 let snitt = document.CreateElement("value");
 snitt.setAttribute("style", "50px");
