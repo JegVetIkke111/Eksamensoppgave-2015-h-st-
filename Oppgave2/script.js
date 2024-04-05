85461c533a23e9c7f5f7b5e611c001a43581f07d
Linossi <145012929+linossi@users.noreply.github.com>
2024-04-05 17:05:13 +0200

    PROMP

--- Oppgave2/script.js
+++ Oppgave2/script.js
@@ -4,5 +4,14 @@ snitt.setAttribute("style", "50px");
 snitt.setAttribute("min", "0");
 document.body.appendChild(snitt);
 
+function buttonClick(snitt, tillegspoeng, grense);
+
+
+//Button
+let button = document.CreateElement("button");
+button.innerHTML = "Studieplass?";
+button.onclick = function(){
+}
+document.body.appendChild(button);
 
 <input style="width:50px" min="0" value="0" type="number" id="antVoks"> 
