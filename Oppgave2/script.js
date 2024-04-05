d1b97b2d50da258ac63e31d1f13497c70decc40b
Boink <98953747+JegVetIkke111@users.noreply.github.com>
2024-04-05 16:19:58 +0200

    save

--- Oppgave2/script.js
+++ Oppgave2/script.js
@@ -2,6 +2,8 @@
 function finnSnitt();
 let snitt = document.CreateElement("Value");
 snitt.setAttribute("style", "50px");
+snitt.setAttribute("min", "0");
+document.body.appendChild(snitt);
 
 
 <input style="width:50px" min="0" value="0" type="number" id="antVoks"> 
