6fa9ad0a182dc3514db3da66a1312ecf52bf92ce
Linossi <145012929+linossi@users.noreply.github.com>
2024-04-05 17:14:56 +0200

    poop

---
+++ Oppgave2/script.js
@@ -0,0 +1,22 @@
+//Lager tre brukerinputt
+let snitt = document.CreateElement("Value");
+snitt.setAttribute("style", "50px");
+snitt.setAttribute("min", "0");
+document.body.appendChild(snitt);
+
+function buttonClick();
+let tall1 = snitt.value;
+let tall2 = tillegspoeng.value;
+let sammenlign = grense.value;
+let brukerpoeng = (snitt * 10) + tillegspoeng;
+
+
+
+//Button
+let button = document.CreateElement("button");
+button.innerHTML = "Studieplass?";
+button.onclick = function(){
+}
+document.body.appendChild(button);
+
+<input style="width:50px" min="0" value="0" type="number" id="antVoks"> 
