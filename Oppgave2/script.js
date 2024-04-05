33d58876e23da302fc06fe009110726a60cbabf2
Boink <98953747+JegVetIkke111@users.noreply.github.com>
2024-04-05 16:18:38 +0200

    save

---
+++ Oppgave2/script.js
@@ -0,0 +1,7 @@
+//Lager tre brukerinputt
+function finnSnitt();
+let snitt = document.CreateElement("Value");
+snitt.setAttribute("style", "50px");
+
+
+<input style="width:50px" min="0" value="0" type="number" id="antVoks"> 
