07fff32c9eb73e7587c3fbad6e2cc09b93dd94da
Boink <98953747+JegVetIkke111@users.noreply.github.com>
2024-04-05 18:18:04 +0200

    save

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-//Lager overskrift for å informere brukeren 
+// Lager overskrift for å informere brukeren 
 let overskrift = document.createElement("h1");
-overskrift.textContent = "Regn ut om du har nok snitt for å komme inn på ditt studie";
+overskrift.textContent = "Regn ut om du har nok snitt for å komme inn på ditt studiepoeng";
 document.body.appendChild(overskrift);
 
-//Lager tre brukerinputt
+// Lager tre brukerinputt
 let snitt = document.createElement("input");
-snitt.setAttribute("style", "50px");
+snitt.setAttribute("style", "width: 50px");
 snitt.setAttribute("min", "0");
 document.body.appendChild(snitt);
 
 let tillegspoeng = document.createElement("input");
-tillegspoeng.setAttribute("style", "50px");
+tillegspoeng.setAttribute("style", "width: 50px");
 tillegspoeng.setAttribute("min", "0");
 document.body.appendChild(tillegspoeng);
 
 let grense = document.createElement("input");
-grense.setAttribute("style", "50px");
+grense.setAttribute("style", "width: 50px");
 grense.setAttribute("min", "0");
 document.body.appendChild(grense);
 
 function buttonClick() {
-    let tall1 = snitt.value;
-    let tall2 = tillegspoeng.value;
-    let sammenlign = grense.value;
-    let brukerpoeng = (snitt * 10) + tillegspoeng;
+    let tall1 = parseFloat(snitt.value);
+    let tall2 = parseFloat(tillegspoeng.value);
+    let sammenlign = parseFloat(grense.value);
+    let brukerpoeng = (tall1 * 10) + tall2;
 
-    if (brukerpoeng < grense) {
-        let resultatMindre.innerHTML = "Du e for dårlig, ha det taper!"; 
-    } else if (brukerpoeng === grense) {
-    let resultatErlik.innerHTML = "Du klarte det akkurat, prøv bedre neste gang ...";
-    }
-    else (brukerpoeng > grense) {
-        let resultatStørre.innerHTML = "Ja! Yippie! Wie cool ist das bitte?!";
+    if (brukerpoeng < sammenlign) {
+        document.getElementById("resultatMindre").textContent = "Du er for dårlig, ha det taper!";
+    } else if (brukerpoeng === sammenlign) {
+        document.getElementById("resultatErlik").textContent = "Du klarte det akkurat, prøv bedre neste gang ...";
+    } else {
+        document.getElementById("resultatStørre").textContent = "Ja! Yippie! Wie cool ist das bitte?!";
     }
 }
 
-//Button
-let button = document.CreateElement("button");
+// Button
+let button = document.createElement("button");
 button.innerHTML = "Studieplass?";
-button.onclick = buttonClick(){
-
-}
+button.onclick = buttonClick;
 document.body.appendChild(button);
-
